878e14d338cf4272464a1d394b9aba11264c21c9
sweetticket <yk494@cornell.edu>
2015-04-27 18:22:02 -0400

    starting

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Cornell FSAE</title>
+    <!--<link rel="stylesheet" type="text/css" href="css/all.css">-->
+    <script src="js/jquery.js"></script>
+    <script src="js/packery.pkgd.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/all.css">
+</head>
+
+<body>
+<?php include("template/nav.php"); ?>
+
+
+</body>
+</html>
---
+++ template/nav.php
@@ -0,0 +1,13 @@
+<nav>
+     <a id="nav_title" href="index.php"><h1>Cornell FSAE</h1></a>
+    <ul>
+            <li><a class="nav_link" href="index.php">HOME</a></li>
+            <li><a class="nav_link" href="login.php">LOGIN</a></li>
+            <li><a class="nav_link" href="about.php">ABOUT</a></li>
+            <li><a class="nav_link" href="news.php">NEWS</a></li>
+            <li><a class="nav_link" href="gallery.php">GALLERY</a></li>
+            <li><a class="nav_link" href="sponsors.php">SPONSORS</a></li>
+            <li><a class="nav_link" href="donate.php">DONATE</a></li>
+            <li><a class="nav_link" href="contact.php">CONTACT</a></li>
+    </ul>
+</nav>
