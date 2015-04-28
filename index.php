d5303d031c2263df93d0aaf8bb07a4ef701dc6d5
sweetticket <yk494@cornell.edu>
2015-04-27 23:46:17 -0400

    drop down fixed menu

--- index.php
+++ index.php
@@ -6,13 +6,13 @@
     <title>Cornell FSAE</title>
     <!--<link rel="stylesheet" type="text/css" href="css/all.css">-->
     <script src="js/jquery.js"></script>
-    <script src="js/packery.pkgd.min.js"></script>
     <link rel="stylesheet" type="text/css" href="css/all.css">
 </head>
 
 <body>
 <?php include("template/nav.php"); ?>
-
+<div class="wrapper">
+</div>
 
 </body>
 </html>
--- template/nav.php
+++ template/nav.php
@@ -3,8 +3,20 @@
     <ul>
             <li><a class="nav_link" href="index.php">HOME</a></li>
             <li><a class="nav_link" href="login.php">LOGIN</a></li>
-            <li><a class="nav_link" href="about.php">ABOUT</a></li>
-            <li><a class="nav_link" href="news.php">NEWS</a></li>
+            <li class="has_sub"><a class="nav_link" href="about.php">ABOUT</a>
+                <ul class="sub_menu">
+                    <li class="nav_sub"><a class="nav_link nav_sub" href="team.php">The Team</a></li>
+                    <li class="nav_sub"><a class="nav_link nav_sub" href="competition.php">The Competition</a></li>
+                    <li class="nav_sub"><a class="nav_link nav_sub" href="cars.php">The Cars</a></li>
+                    <li class="nav_sub"><a class="nav_link nav_sub" href="history.php">Our History</a></li>
+                </ul>
+            </li>
+            <li class="has_sub"><a class="nav_link" href="news.php">NEWS</a>
+                <ul class="sub_menu">
+                    <li class="nav_sub"><a class="nav_link nav_sub" href="annouce.php">Announcements</a></li>
+                    <li class="nav_sub"><a class="nav_link nav_sub" href="newsletters.php">Newsletters</a></li>
+                </ul>
+            </li>
             <li><a class="nav_link" href="gallery.php">GALLERY</a></li>
             <li><a class="nav_link" href="sponsors.php">SPONSORS</a></li>
             <li><a class="nav_link" href="donate.php">DONATE</a></li>
