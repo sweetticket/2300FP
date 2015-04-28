3382ebd14af7dfbf3238936e98a054dcbb770b83
sweetticket <yk494@cornell.edu>
2015-04-28 13:13:25 -0400

    added imgs and sponsors to header

--- template/header.php
+++ template/header.php
@@ -1,3 +1,14 @@
 <header>
-<a href="index.php" class="header_link"><img src="img/logo.jpg" alt="logo" class="header_img"></a>
+	<div class="left logo_container">
+		<a href="index.php" class="header_link"><img src="img/logo.jpg" alt="logo" class="header_img"></a>
+	</div>
+	<div class="sponsors_header right">
+		<div class="sponsors_row">
+			<a href="sponsors.php"><img src="img/sponsors/title1_destiny.jpg" class="sponsor_in_header"></a>
+			<a href="sponsors.php"><img src="img/sponsors/title2_watkins.jpg" class="sponsor_in_header"></a>
+		</div>
+		<div class="sponsors_row">	
+			<a href="sponsors.php"><img src="img/sponsors/title3_toray.jpg" class="sponsor_in_header"></a>
+		</div>
+	</div>
 </header>
