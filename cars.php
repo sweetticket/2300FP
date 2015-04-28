724f4eaa1f511895b39ef9da477323866641ec2e
sweetticket <yk494@cornell.edu>
2015-04-28 15:33:05 -0400

    rough 'cars' page, should fix side menu later

--- cars.php
+++ cars.php
@@ -8,6 +8,7 @@
     <!--<link rel="stylesheet" type="text/css" href="css/all.css">-->
     <script src="js/jquery.js"></script>
     <link rel="stylesheet" type="text/css" href="css/all.css">
+    <link rel="stylesheet" type="text/css" href="css/cars.css">
 </head>
 
 <body>
@@ -17,6 +18,56 @@
 <div class="main_content_wrapper">
 		<div class="landing_img_container">
 			<img src="" alt="" class="landing_img"> <!-- landing img will go here-->
+		</div>
+		<div class="side_nav">
+			<ul>
+				<li><a href="#current" class="car_nav_link">The Current Car</a></li>
+				<li><a href="#2010s" class="car_nav_link">2010s</a>
+					<ul>
+						<li><a id="2015" href="#current" class="car_nav_link">2015</a></li>
+						<li><a id="2014" href="#" class="car_nav_link">2014</a></li>
+						<li><a id="2013" href="#" class="car_nav_link">2013</a></li>
+						<li><a id="2012" href="#" class="car_nav_link">2012</a></li>
+						<li><a id="2011" href="#" class="car_nav_link">2011</a></li>
+						<li><a id="2010" href="#" class="car_nav_link">2010</a></li>
+					</ul>
+				</li>
+				<li><a href="#2000s" class="car_nav_link">2000s</a>
+					<ul>
+						<li><a id="2009" href="#" class="car_nav_link">2009</a></li>
+						<li><a id="2008" href="#" class="car_nav_link">2008</a></li>
+						<li><a id="2007" href="#" class="car_nav_link">2007</a></li>
+						<li><a id="2006" href="#" class="car_nav_link">2006</a></li>
+						<li><a id="2005" href="#" class="car_nav_link">2005</a></li>
+						<li><a id="2004" href="#" class="car_nav_link">2004</a></li>
+						<li><a id="2003" href="#" class="car_nav_link">2003</a></li>
+						<li><a id="2002" href="#" class="car_nav_link">2002</a></li>
+						<li><a id="2001" href="#" class="car_nav_link">2001</a></li>
+						<li><a id="2000" href="#" class="car_nav_link">2000</a></li>
+					</ul>
+				</li>
+				<li><a href="#1990s" class="car_nav_link">1990s</a>
+					<ul>
+						<li><a id="1999" href="#" class="car_nav_link">1999</a></li>
+						<li><a id="1998" href="#" class="car_nav_link">1998</a></li>
+						<li><a id="1997" href="#" class="car_nav_link">1997</a></li>
+						<li><a id="1996" href="#" class="car_nav_link">1996</a></li>
+						<li><a id="1995" href="#" class="car_nav_link">1995</a></li>
+						<li><a id="1994" href="#" class="car_nav_link">1994</a></li>
+						<li><a id="1993" href="#" class="car_nav_link">1993</a></li>
+						<li><a id="1992" href="#" class="car_nav_link">1992</a></li>
+						<li><a id="1991" href="#" class="car_nav_link">1991</a></li>
+						<li><a id="1990" href="#" class="car_nav_link">1990</a></li>
+					</ul>
+				</li>
+				<li><a href="#1980s" class="car_nav_link">1980s</a>
+					<ul>
+						<li><a id="1989" href="#" class="car_nav_link">1989</a></li>
+						<li><a id="1988" href="#" class="car_nav_link">1988</a></li>
+						<li><a id="1987" href="#" class="car_nav_link">1987</a></li>
+					</ul>
+				</li>
+			</ul>
 		</div>
 		<div class="main_content">
 			<h1>The Cars</h1>
