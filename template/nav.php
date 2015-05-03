61783a9471555c94ec8cd98b458dbc339c8949b9
sweetticket <yk494@cornell.edu>
2015-05-03 16:53:29 -0400

    prettier nav

--- template/nav.php
+++ template/nav.php
@@ -3,16 +3,16 @@
             <li><a class="nav_link" href="index.php">HOME</a></li>
             <li class="has_sub"><a class="nav_link" href="about.php">ABOUT</a>
                 <ul class="sub_menu">
-                    <li class="nav_sub"><a class="nav_link nav_sub" href="team.php">The Team</a></li>
-                    <li class="nav_sub"><a class="nav_link nav_sub" href="competition.php">The Competition</a></li>
-                    <li class="nav_sub"><a class="nav_link nav_sub" href="cars.php">The Cars</a></li>
-                    <li class="nav_sub"><a class="nav_link nav_sub" href="history.php">Our History</a></li>
+                    <li class="nav_sub"><a class="nav_sub_link" href="team.php">The Team</a></li>
+                    <li class="nav_sub"><a class="nav_sub_link" href="competition.php">The Competition</a></li>
+                    <li class="nav_sub"><a class="nav_sub_link" href="cars.php">The Cars</a></li>
+                    <li class="nav_sub"><a class="nav_sub_link" href="history.php">Our History</a></li>
                 </ul>
             </li>
             <li class="has_sub"><a class="nav_link" href="news.php">NEWS</a>
                 <ul class="sub_menu">
-                    <li class="nav_sub"><a class="nav_link nav_sub" href="announce.php">Announcements</a></li>
-                    <li class="nav_sub"><a class="nav_link nav_sub" href="newsletters.php">Newsletters</a></li>
+                    <li class="nav_sub"><a class="nav_sub_link" href="announce.php">Announcements</a></li>
+                    <li class="nav_sub"><a class="nav_sub_link" href="newsletters.php">Newsletters</a></li>
                 </ul>
             </li>
             <li><a class="nav_link" href="gallery.php">GALLERY</a></li>
