77611562b9dbf3dc07d3a9afa2879a8c1a0bc6cf
wxy1224 <xw288@cornell.edu>
2015-05-10 15:49:03 -0400

    update gallery
    add images and add some style

--- gallery.php
+++ gallery.php
@@ -9,6 +9,7 @@
     <script src="js/jquery.js"></script>
     <script src="js/packery.pkgd.min.js"></script>
     <link rel="stylesheet" type="text/css" href="css/all.css">
+    <link rel="stylesheet" type="text/css" href="css/gallery.css">
 </head>
 
 <body>
@@ -27,6 +28,19 @@
             // Alternatively this feature may be done using javascript, which would be done in a separate js file
             // JavaScript would make it easier to display all the images organized by albums on one page
             ?>
+
+           
+            <img src="img/gallery/the_team/2011.jpg" alt="team2011">
+            <img src="img/gallery/the_team/fa_2012.jpg" alt="team2012">
+            <img src="img/gallery/the_team/sp_2013.jpg" alt="team2013"> 
+            <img src="img/gallery/the_car/img1.jpg" alt="car1">
+            <img src="img/gallery/the_car/img2.jpg" alt="car2">
+            <img src="img/gallery/childrenstem.jpg" alt="childrenstem">
+            <img src="img/gallery/sp14recruit.jpg" alt="sp14recruit">
+            <img src="img/gallery/newsletter1.jpg" alt="newsletter1">
+            <img src="img/gallery/newsletter.jpg" alt="newsletter">
+            <img src="img/gallery/2013_accel_win.png" alt="newsletter">
+
 		</div>
 
 </div>
