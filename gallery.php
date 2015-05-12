7210e5b60a47743c21b3438474d817d4e9db6200
sweetticket <yk494@cornell.edu>
2015-05-12 16:16:49 -0400

    styled gallery

--- gallery.php
+++ gallery.php
@@ -25,29 +25,51 @@ $headersrc = "./img/headers/header_gallery.png"; ?>
 		<div id="main_content">
 			<h1>Gallery</h1>
             <p>Images will go here.</p>
-            <?php
-            // This page will load content from the database using php and display all the album information for view
-            // Alternatively this feature may be done using javascript, which would be done in a separate js file
-            // JavaScript would make it easier to display all the images organized by albums on one page
-            ?>
+            <div id="container">
+            
+            <div class="item_container">
+            <a href="img/gallery/the_team/2011.jpg"><img src="img/gallery/the_team/2011.jpg" alt="team2011"></a>
+            </div>
 
-           
-            <img src="img/gallery/the_team/2011.jpg" alt="team2011">
-            <img src="img/gallery/the_team/fa_2012.jpg" alt="team2012">
-            <img src="img/gallery/the_team/sp_2013.jpg" alt="team2013"> 
-            <img src="img/gallery/the_car/img1.jpg" alt="car1">
-            <img src="img/gallery/the_car/img2.jpg" alt="car2">
-            <img src="img/gallery/childrenstem.jpg" alt="childrenstem">
-            <img src="img/gallery/sp14recruit.jpg" alt="sp14recruit">
-            <img src="img/gallery/newsletter1.jpg" alt="newsletter1">
-            <img src="img/gallery/newsletter.jpg" alt="newsletter">
-            <img src="img/gallery/2013_accel_win.png" alt="newsletter">
+            <div class="item_container">
+            <a href="img/gallery/the_team/sp_2013.jpg"><img src="img/gallery/the_team/sp_2013.jpg" alt="team2013"></a>
+            </div>
+
+            <div class="item_container">
+            <a href="img/gallery/the_car/img1.jpg"><img src="img/gallery/the_car/img1.jpg" alt="car1"></a>
+            </div>
+
+            <div class="item_container">
+            <a href="img/gallery/the_car/img2.jpg"><img src="img/gallery/the_car/img2.jpg" alt="car2"></a>
+            </div>
+
+            <div class="item_container">
+            <a href="img/gallery/childrenstem.jpg"><img src="img/gallery/childrenstem.jpg" alt="childrenstem"></a>
+            </div>
+
+            <div class="item_container">
+            <a href="img/gallery/sp14recruit.jpg"><img src="img/gallery/sp14recruit.jpg" alt="sp14recruit"></a>
+            </div>
+
+            <div class="item_container">
+            <a href="img/gallery/newsletter1.jpg"><img src="img/gallery/newsletter1.jpg" alt="newsletter1"></a>
+            </div>
+
+            <div class="item_container">
+            <a href="img/gallery/newsletter.jpg"><img src="img/gallery/newsletter.jpg" alt="newsletter"></a>
+            </div>
+
+            <div class="item_container">
+            <a href="img/gallery/2013_accel_win.png"><img src="img/gallery/2013_accel_win.png" alt="newsletter"></a>
+            </div>
+
+            </div>
 
 		</div>
 
 </div>
 <?php include("template/footer.php"); ?>
 </div>
-<!--<script src="js/display_data.js"></script>-->
+<script src="js/display_data.js"></script>
 </body>
 </html>
